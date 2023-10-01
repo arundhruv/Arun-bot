b95b0cb293c2e23e9a0096159d39d2a12dc893d9
Arun Dhruv <131136380+arundhruv@users.noreply.github.com>
2023-10-02 02:26:58 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
-SESSION      = ""
-DATABASE_URI = ""
+API_ID       = 21295053
+API_HASH     = "297598578931dcc642c2519414079f8e"
+BOT_TOKEN    = "6346538148:AAGYQLjUF98i8NLAE0wgo5xv6BOC5RKaWPo"
+DATABASE_URI = "mongodb+srv://Arun:Arun@cluster0.zasxoq5.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1001884373226
-ADMIN        = 1350642629
-CHANNEL      = "@GreyMatter_Bots"
+ADMIN        = 6011524145
+CHANNEL      = "@Rose_filterr_bot"
+
